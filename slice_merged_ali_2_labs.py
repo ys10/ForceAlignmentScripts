8be7d056d225170c5f2eb5f7341adf5a8253ffe5
yangshuai <yangshuai@pachiratech.com>
2017-11-18 16:20:51 +0800

    Slice merged alignment result into some sentence lab files.

---
+++ slice_merged_ali_2_labs.py
@@ -0,0 +1,27 @@
+# coding: UTF-8
+
+merged_align_file_name = "test/final_align_test.txt"
+sentence_dict = dict() # A dictionary of  sentence lists, each list contains some phonemes with their begin & end time.
+with open(merged_align_file_name, "r") as merged_align_file:
+    while 1:
+        lines = merged_align_file.readlines(10000)
+        if not lines:
+            break
+        for line in lines:
+            phoneme_list = line.split('\t\t')
+            print(phoneme_list)
+            if phoneme_list[0] not in sentence_dict.keys():
+                new_phoneme_list = list()
+                sentence_dict[phoneme_list[0]] = new_phoneme_list
+                pass
+            sentence_dict[phoneme_list[0]].append("\t".join(phoneme_list[1:]))
+            pass
+        pass
+    pass
+
+labs_dir_name = "labs/"
+for sentence_file_name in sentence_dict.keys():
+    with open(labs_dir_name + sentence_file_name, "w") as sentence_file:
+        sentence_file.writelines(sentence_dict[sentence_file_name])
+        pass
+    pass
